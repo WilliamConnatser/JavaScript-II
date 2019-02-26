f7dd3431318cb68ef43cff2c72b8284fcf7d8d85
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-02-26 17:50:08 -0600

    finished stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -17,15 +17,32 @@ lambdaSlack();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let counter = 1;
   // Return a function that when invoked increments and returns a counter variable.
+  return () => counter++;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counter = 1;
+
+  return {
+    increment: () => counter++,
+    decrement: () => counter--
+  }
 };
+
+const newCounterFactory = counterFactory();
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.decrement());
