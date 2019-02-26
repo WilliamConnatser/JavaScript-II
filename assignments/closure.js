6412761020d40185e4bbd578362a4b77695b3a8d
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-02-26 16:21:05 -0600

    finished callbacks

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function lambdaSlack() {
+  function generalChat() {
+    function web18() {
+      
+    }
+  }
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
